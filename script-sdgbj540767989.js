30438dc06ed150614ce1c1f4ec96ea5d5a9c1332
elsoghiar <mahmoudhouseen555@gmail.com>
2024-12-06 06:57:42 +0200

    Update script-sdgbj540767989.js

--- script-sdgbj540767989.js
+++ script-sdgbj540767989.js
@@ -2785,25 +2785,8 @@ window.addEventListener('load', () => {
 
 //////////////////////////////////////
 
-const skyContainer = document.querySelector(".sky-container");
 
-// تقليل عدد النجوم
-const starCount = 50;
-
-for (let i = 0; i < starCount; i++) {
-  const star = document.createElement("div");
-  star.classList.add("star");
-
-  // وضع عشوائي للنجوم
-  star.style.left = Math.random() * 100 + "vw";
-  star.style.top = Math.random() * 100 + "vh";
-  star.style.animationDelay = Math.random() * 10 + "s"; // تأخير عشوائي لظهور النجوم
-  star.style.animationDuration = Math.random() * 15 + 15 + "s"; // مدة حركة النجوم
-
-  skyContainer.appendChild(star);
-}
-
-
+eruda.init();
 
 // تفعيل التطبيق
 initializeApp();
