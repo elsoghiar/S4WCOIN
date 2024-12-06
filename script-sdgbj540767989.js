570c47cc93f2a2078f1348eb3c5e67de3c164c99
elsoghiar <mahmoudhouseen555@gmail.com>
2024-12-06 05:39:37 +0200

    Update script-sdgbj540767989.js

--- script-sdgbj540767989.js
+++ script-sdgbj540767989.js
@@ -2785,6 +2785,25 @@ window.addEventListener('load', () => {
 
 //////////////////////////////////////
 
+const starsContainer = document.createElement("div");
+starsContainer.classList.add("stars");
+
+for (let i = 0; i < 100; i++) { // عدد النجوم
+  const star = document.createElement("div");
+  star.classList.add("star");
+
+  // وضع عشوائي للنجوم
+  star.style.left = Math.random() * 100 + "%";
+  star.style.top = Math.random() * 100 + "%";
+  star.style.animationDelay = Math.random() * 5 + "s";
+  star.style.animationDuration = Math.random() * 5 + 5 + "s";
+
+  starsContainer.appendChild(star);
+}
+
+document.querySelector(".minee-container").appendChild(starsContainer);
+
+
 
 
 
